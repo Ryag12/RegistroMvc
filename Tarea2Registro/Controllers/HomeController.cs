4f55af9f5305b65786bb666f1f9ce04d9dc954e9
Roberti Amarante <yavier.ag@gmail.com>
2017-05-14 12:07:54 -0400

    commin

---
+++ Tarea2Registro/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Tarea2Registro.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Medicinas()
+        {
+            ViewBag.Message = "Elige tu Medicina.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Pagina de Contacto.";
+
+            return View();
+        }
+    }
+}
---
+++ Tarea2Registro/DAL/MiMedicinaDB.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using Tarea2Registro.Models;
+
+namespace Tarea2Registro.DAL
+{
+    public class MiMedicinaDB: DbContext
+    {
+        public MiMedicinaDB(): base("ConStr")
+        {
+                
+        }
+        public DbSet<Medicinas> Agregar { get; set; }
+    }
+}
